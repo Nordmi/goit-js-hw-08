71a8eed683488328a419b671d8694e502fe48dfe
Anton Taran <antondp23@gmail.com>
2022-11-19 21:01:06 +0200

    add first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryListEl = document.querySelector('.gallery');
+
+function createMarkUp(obj) {
+  return obj
+    .map(
+      ({
+        preview,
+        original,
+        description,
+      }) => `<li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>`
+    )
+    .join('');
+}
+galleryListEl.innerHTML = createMarkUp(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: '250',
+});
 
 console.log(galleryItems);
