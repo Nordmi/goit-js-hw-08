b1fd59bc4a480686ec0139d51723803db8afc739
Anton Taran <antondp23@gmail.com>
2022-11-19 22:23:40 +0200

    2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const TIME_KEY = 'videoplayer-current-time';
+
+checkTime();
+
+const onPlay = function (data) {
+  localStorage.setItem(TIME_KEY, data.seconds);
+  // console.log(localStorage.getItem(TIME_KEY));
+};
+
+function checkTime() {
+  if (localStorage.getItem(TIME_KEY) === null) {
+    return localStorage.setItem(TIME_KEY, 0);
+  }
+  player.setCurrentTime(localStorage.getItem(TIME_KEY));
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
